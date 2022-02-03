e7d94607c09baaeb81fb6d9a2405cba9d7842d14
ericpkatz <ericpkatz@gmail.com>
2022-02-02 19:53:58 -0500

    added models

--- spec.js
+++ spec.js
@@ -1,9 +1,33 @@
 const { expect } = require('chai');
+
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
+const { DataTypes: { STRING }} = Sequelize;
+
+const User = conn.define('user', {
+  name: {
+    type: STRING 
+  }
+});
 describe('my app', ()=> {
+  beforeEach(async()=> {
+    await conn.sync({ force: true });
+  });
+  beforeEach(async()=> {
+    await User.create({ name: 'moe' });
+  });
   it('true equals true', ()=> {
     expect(true).equals(true);
   });
   it('true equals true', ()=> {
     expect(true).equals(true);
+  });
+  it('moe exists', async()=> {
+    const moe = await User.findOne({
+      where: {
+        name: 'moe'
+      }
+    });
+    expect(moe.name).to.equal('moe');
   });
 });
